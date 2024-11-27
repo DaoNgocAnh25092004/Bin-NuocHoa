324d4fd7397a9b4292b301c08a57b26e0a8c863a
DaoNgocAnh25092004 <Daongocanh25042004@gmail.com>
2024-11-27 23:22:59 +0700

    first commit

---
+++ Js/data.js
@@ -0,0 +1,12 @@
+var data = [
+    {
+        name : 'Armaf Club de Nuit Intense Man',
+        chuyenTrang : '1'
+    },
+    {
+        name : 'Versace Eros For Men',
+        chuyenTrang : '2'
+    },
+]
+
+
